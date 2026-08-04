408f8657bdc1c8382ef7463bab58cd465a589244
rohit_mishr <rohitmishra.rm583@gmail.com>
2026-08-05 00:27:52 +0530

    led toggle test

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,40 +1,33 @@
 #include <stdint.h>
 
-// We will assume the main application firmware is flashed starting at Sector 2
-#define MAIN_APP_START_ADDRESS 0x08008000
-#define SCB_VTOR_ADDRESS       0xE000ED08
+// RCC Registers
+#define RCC_BASE       0x40023800
+#define RCC_AHB1ENR    *(volatile uint32_t*)(RCC_BASE + 0x30)
 
-void jump_to_application(void) {
-    // Step A: Read the main application's Stack Pointer
-    uint32_t app_msp_value = *(volatile uint32_t*)MAIN_APP_START_ADDRESS;
-    
-    // Step B: Read the main application's Reset Handler
-    uint32_t app_jump_address = *(volatile uint32_t*)(MAIN_APP_START_ADDRESS + 4);
-    
-    // Step C: Cast to a C function pointer
-    void (*app_reset_handler)(void) = (void (*)(void))app_jump_address;
-    
-    //Step D : set the vtor to the main app's address
-    *(volatile uint32_t*)SCB_VTOR_ADDRESS = MAIN_APP_START_ADDRESS;
-    
-    // Step E: Physically set the ARM CPU's Main Stack Pointer (MSP)
-    __asm volatile("msr msp, %0" : : "r" (app_msp_value));
-
-    // Step F: Jump! 
-    app_reset_handler();
-}
+// GPIOA Registers
+#define GPIOA_BASE     0x40020000
+#define GPIOA_MODER    *(volatile uint32_t*)(GPIOA_BASE + 0x00)
+#define GPIOA_ODR      *(volatile uint32_t*)(GPIOA_BASE + 0x14)
 
 int main(void) {
-    // 1. TODO: Initialize Clocks and UART
-    // 2. TODO: Check if the user is requesting a firmware update
+    // 1. Enable GPIOA Clock
+    RCC_AHB1ENR |= (1 << 0);
     
-    // 3. TODO: De-initialize clocks and UART before jumping
+    // 2. Set PA5 as Output (Bits 10:11 = 01)
+    // First, clear the two bits just to be safe
+    GPIOA_MODER &= ~(3 << 10); 
+    // Then set bit 10 to 1
+    GPIOA_MODER |= (1 << 10);  
     
-    // 4. Jump to the main application
-    jump_to_application();
-    
-    // The CPU has left the bootloader. It will never reach this while loop.
+    // 3. Infinite Blinky Loop
     while (1) {
+        // Toggle PA5
+        GPIOA_ODR ^= (1 << 5);
+        
+        // Dumb Delay Loop (MUST be volatile!)
+        for (volatile uint32_t i = 0; i < 500000; i++) {
+            // Do nothing, just burn CPU cycles
+        }
     }
     
     return 0;
